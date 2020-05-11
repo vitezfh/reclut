d5f2d467a8005ceff272dd1ca9f7fc9ada3fbae1
Stjepan Vitez <vitezfh@gmail.com>
2020-05-11 13:08:07 +0200

    add tests for simple utilities

---
+++ test/test_downloader.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+import os
+import sys
+
+from reclut.downloader import Downloader
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+def test():
+    assert True == False
+
+
---
+++ test/test_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+import os
+import sys
+
+from reclut.utils import get_extension, sanitize_title
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+def test_get_extension():
+    assert get_extension("https://www.foo.bar/get.this/file.ext") == "ext"
+
+
+def test_sanitize_title():
+    assert sanitize_title("I took a few shots at Lake Louise today and Google offered me this panorama:") \
+           == "I_took_a_few_shots_at_Lake_Louise_today_and_Google_offered_me_this_panorama:"
+    assert sanitize_title(" - -.,/!?()[] ") == "___"
